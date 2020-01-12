b78f8a7143e51710f8f554325a2461eabbd3cb81
sampath surineni <sampath.skt@gmail.com>
2020-01-12 12:16:59 +0530

    added avy and yasnippet

--- personal/my_packages.el
+++ personal/my_packages.el
@@ -1,17 +1,30 @@
-(prelude-require-packages '(deadgrep default-text-scale
+(prelude-require-packages '(avy deadgrep
+                                default-text-scale
 				     monokai-theme
-                                     yasnippet))
+                                     yasnippet yasnippet-snippets))
 
 
-(yas-global-mode 1)
+
 ;; global
 (global-unset-key (kbd "C-z"))
+
+;; avy
+(global-set-key (kbd "C-:") 'avy-goto-char)
+
 ;; company-mode
 (setq company-idle-delay 0)
 (define-key company-mode-map (kbd "<backtab>") 'company-complete)
 ;; (define-key python-mode-map (kbd "<backtab>") nil)
 ;; dead grep
+
 (global-set-key (kbd "s-f") 'deadgrep)
 
 (default-text-scale-mode t)
 (global-hl-line-mode -1)
+
+(yas-global-mode 1)
+
+(setenv "LANG" "en_US.UTF-8")
+(setenv "LC_ALL" "en_US.UTF-8")
+(setenv "LC_CTYPE" "en_US.UTF-8")
+(setenv "PYTHONIOENCODING" "utf-8")
