96c0c8ebe7d01b8750ff7c731292b594aac1c825
sampath surineni <sampath.skt@gmail.com>
2020-04-12 17:44:34 +0530

    Unbound keys and rebound to proper keys

---
+++ personal/1-my-packages.el
@@ -0,0 +1,32 @@
+(prelude-require-packages '(avy deadgrep
+                                default-text-scale
+				     monokai-theme
+                                     yasnippet yasnippet-snippets))
+
+
+
+;; global
+(global-unset-key (kbd "C-z"))
+
+;; avy
+(global-set-key (kbd "C-;") 'avy-goto-char-timer)
+
+
+;; company-mode
+(setq company-idle-delay 0)
+(define-key company-mode-map (kbd "<backtab>") 'company-complete)
+;; (define-key python-mode-map (kbd "<backtab>") nil)
+;; dead grep
+
+
+
+(default-text-scale-mode t)
+(global-hl-line-mode -1)
+
+(yas-global-mode 1)
+
+(setenv "LANG" "en_US.UTF-8")
+(setenv "LC_ALL" "en_US.UTF-8")
+(setenv "LC_CTYPE" "en_US.UTF-8")
+(setenv "PYTHONIOENCODING" "utf-8")
+(set-default-coding-systems 'utf-8)
--- personal/2-key-config.el
+++ personal/2-key-config.el
@@ -1,27 +1,8 @@
-(prelude-require-packages '(avy deadgrep
-                                default-text-scale
-				     monokai-theme
-                                     yasnippet yasnippet-snippets))
-
-
-
-;; global
-(global-unset-key (kbd "C-z"))
-
-;; avy
-(global-set-key (kbd "C-;") 'avy-goto-char-timer)
-
-
-;; company-mode
-(setq company-idle-delay 0)
-(define-key company-mode-map (kbd "<backtab>") 'company-complete)
-;; (define-key python-mode-map (kbd "<backtab>") nil)
-;; dead grep
-
 (global-set-key (kbd "RET") 'newline-and-indent)
 
 (global-set-key (kbd "C--") 'split-window-below)
 (global-set-key (kbd "C-\\") 'split-window-right)
+(define-key undo-tree-map (kbd "C-/") nil)
 (global-set-key (kbd "C-/") 'comment-line)
 
 (global-set-key (kbd "C-b") 'backward-word)
@@ -40,21 +21,11 @@
 (global-set-key (kbd "M-b") 'backward-char)
 (global-set-key (kbd "M-d") 'delete-char)
 (global-set-key (kbd "M-e") 'end-of-buffer)
+(define-key prelude-mode-map (kbd "M-o") nil)
+
 (global-set-key (kbd "M-o") 'ace-window)
 (global-set-key (kbd "M-f") 'forward-char)
 
 (global-set-key (kbd "s-f") 'deadgrep)
 
 (define-key lisp-interaction-mode-map (kbd "C-j") nil)
-
-
-(default-text-scale-mode t)
-(global-hl-line-mode -1)
-
-(yas-global-mode 1)
-
-(setenv "LANG" "en_US.UTF-8")
-(setenv "LC_ALL" "en_US.UTF-8")
-(setenv "LC_CTYPE" "en_US.UTF-8")
-(setenv "PYTHONIOENCODING" "utf-8")
-(set-default-coding-systems 'utf-8)
