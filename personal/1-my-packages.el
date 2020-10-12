ceb343f763ad3f92823b5d010369c6aa687e134e
Sampath Surineni <sampath.skt@gmail.com>
2020-10-12 20:19:46 +0530

    disabled unncessary info

--- personal/1-my-packages.el
+++ personal/1-my-packages.el
@@ -33,8 +33,13 @@
 ;; smart model line related
 (setq sml/no-confirm-load-theme t)
 (sml/setup)
-(setq size-indication-mode nil)
+;; (setq size-indication-mode nil)
 ;; (setq sml/shorten-directory t)
+(setq sml/size-indication-format nil)
+(setq sml/position-percentage-format nil)
+(setq sml/name-width 10)
+;; (setq sml/use-projectile-p nil)
+
 (setq sml/shorten-modes t)
 (setq sml/shortener-func (lambda (_dir _max-length) ""))
 (setq rm-blacklist
